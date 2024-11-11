cbc939d839744edebbd1cd5fc9688f916b142971
ldcnetto <ldcn@cin.ufpe.br>
2024-11-11 03:28:48 -0300

    refactor: att import of the global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './global.css';
+import './styles/global.css';
 import App from './components/HeaderSearchBar/headerSearchBar.tsx';
 import SearchBooks from './services/SearchBooks.tsx';
 
