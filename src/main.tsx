a7d0b557ccbb10bc48ea1f2a713b0aeccbc6204a
Leônidas Netto <ldcn@cin.ufpe.br>
2024-11-11 03:51:42 -0300

    Header finalizado (#3)
    * feat: header with search bar
    
    * feat: service search
    
    * refactor: Nunito for the tags p and oswald for h
    
    * feat: prototiping the new design
    
    * build: installing postcss
    
    * refactor: global moved to styles paste
    
    * refactor: including darkMode and adjustments at p tag
    
    * refactor: att import of the global.css
    
    * feat: basicRating component
    
    * feat: grid, search and cards finished
    
    * refactor: including dark mode icon in the header
    
    * feat: searching with enter button

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './global.css';
+import './styles/global.css';
 import App from './components/HeaderSearchBar/headerSearchBar.tsx';
 import SearchBooks from './services/SearchBooks.tsx';
 
@@ -13,5 +13,12 @@ createRoot(document.getElementById('root')!).render(
       </div>
       <div className="max-w-[50%] w-full"></div>
     </div>
+    <div className="flex flex-row w-full">
+      <div className="flex flex-col max-w-[50%]">
+        <App />
+      </div>
+      <div className="max-w-[50%] w-full"></div>
+    </div>
   </StrictMode>,
+);
 );
