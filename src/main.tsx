0fbcca10d4ab8dc34001ec5110dd6c176ead3aa4
ldcnetto <ldcn@cin.ufpe.br>
2024-11-11 21:18:48 -0300

    feat: att main to show only categories or gridItems

--- src/main.tsx
+++ src/main.tsx
@@ -5,33 +5,53 @@ import { Layout } from './components/Layout/layout.tsx';
 import GridItems from './components/GridItems/gridItems.tsx';
 import Header from './components/Header/header.tsx';
 import { Book } from './services/SearchBooks';
+import GenreFilter from './components/Categories/categories.tsx';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function App() {
   const [books, setBooks] = useState<Book[]>([]);
   const [searchLoading, setSearchLoading] = useState(false);
   const [searchError, setSearchError] = useState<string | null>(null);
   const [appDarkMode, setAppDarkMode] = useState(false);
+  const [showGridItems, setShowGridItems] = useState(false); // Nova flag
 
   const handleDarkModeChange = (isDarkMode) => {
     setAppDarkMode(isDarkMode);
   };
 
+  const isBigScreen = useMediaQuery('(min-width:580px)');
+
+  // Função para controlar a exibição dos componentes
+  const handleSearchResults = (results: Book[]) => {
+    setBooks(results);
+    setShowGridItems(results.length > 0); // Mostra GridItems se houver resultados
+  };
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Header
-        onSearchResults={setBooks}
+        onSearchResults={handleSearchResults} // Passa a nova função
         onSearchLoading={setSearchLoading}
         onSearchError={setSearchError}
         onDarkModeChange={handleDarkModeChange}
       />
-      <div style={{ marginBottom: '80px' }} />
+      {isBigScreen ? (
+        <div style={{ marginBottom: '80px' }} />
+      ) : (
+        <div style={{ marginBottom: '100px' }} />
+      )}
+
       <Layout>
-        <GridItems
-          searchLoading={searchLoading}
-          searchError={searchError}
-          books={books}
-          darkMode={appDarkMode}
-        />
+        {showGridItems ? ( // Renderização condicional
+          <GridItems
+            searchLoading={searchLoading}
+            searchError={searchError}
+            books={books}
+            darkMode={appDarkMode}
+          />
+        ) : (
+          <GenreFilter darkMode={appDarkMode} />
+        )}
       </Layout>
     </div>
   );
